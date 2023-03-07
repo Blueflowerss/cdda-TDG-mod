e5cda0b26dd5fd5b06b7fc7e44550583eec5fc17
Blueflowers <77662682+Blueflowerss@users.noreply.github.com>
2023-03-07 13:42:12 +0000

    Add files via upload

--- The Daily Grind mod/generateMutationforpurple.py
+++ The Daily Grind mod/generateMutationforpurple.py
@@ -1,5 +1,7 @@
+##takes a json files with an array of mutation id's
 import json,os
 mutations = []
+removeMutations = []
 string = ""
 stringList = ""
 mutationList = {}
@@ -16,8 +18,10 @@ for file in os.listdir("../../../current/data/json/mutations"):
 with open("../mutations.json") as f:
     content = f.read()
     jsonContent = json.loads(content)
-    for mut in jsonContent:
+    for mut in jsonContent["add"]:
         mutations.append(mut)
+    for mut in jsonContent["remove"]:
+        removeMutations.append(mut)
 for mut in mutations:
     if mut not in mutationList:
         raise ValueError(mut+" non existant")
@@ -30,4 +34,18 @@ for mut in mutations:
 	],\n\
 	\"false_effect\":[{\"run_eocs\":[\"EOC_purple_orb\"]}]},\n\n"
     stringList += "[ \"EOC_purple_orb_mut_"+mut+"\", {\"const\":45} ],\n"
-print(string)
+string = ""
+stringList = ""
+for mut in removeMutations:
+    if mut not in mutationList:
+        raise ValueError(mut+ "non existent")
+    string += "{\"type\":\"effect_on_condition\",\n\
+    \"id\": \"EOC_purple_orb_mut_remove_"+mut+"\",\n\
+    \"condition\":{\"u_has_trait\":\""+mut+"\"},\n\
+    \"effect\":[\n\
+    {\"u_message\":\"Shell Upgrade - Heal "+mutationList[mut]+" \"},\n\
+    {\"u_lose_trait\":\""+mut+"\"}\n\
+    ],\n\
+    \"false_effect\":[{\"run_eocs\":[\"EOC_purple_orb\"]}]},\n"
+    stringList += "[ \"EOC_purple_orb_mut_remove_"+mut+"\", {\"const\":450} ],\n"
+print(stringList)
